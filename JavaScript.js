e77a44ac25d44c046cf600bfc3cbe037bc17cdf4
Anass Talab <Anass.Talab@education.nsw.gov.au>
2021-06-21 12:36:57 +1000

    Version 1.2
    Made a start button and makes it disappear. also got something started within the code

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+﻿    function buttonTest() {
+        document.getElementById("StartBtn").style.visibility = "hidden";
+    }
+function invisTest() {
+    if (document.getElementById("StartBtn").style.visibility == "hidden") {
+        window.alert(nigeria)
+    } else {
+        window.alert(nigeria)
+    }
+}
+function GmeStrt() {
+    For(document.getElementById("StartBtn").style.visibility == "hidden") {
+        Score = 0
+    }
+}
+var timeout = setTimeout(function () { yourFunction() }, 60000);
+var interval = setInterval(function () { yourFunction() }, 1000);
